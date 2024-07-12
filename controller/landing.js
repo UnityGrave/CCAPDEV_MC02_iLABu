51560f5f5369699abc5389b1064d1c899af7d407
lanztriozo <141170638+lanztriozo@users.noreply.github.com>
2024-07-12 12:02:05 +0800

    Add files via upload

--- controller/landing.js
+++ controller/landing.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-router.get('/', function(req, res) {
+router.get('/landingpage', function(req, res) {
 	res.sendFile(path.join(__dirname + "\\" + "../public/index.html"));
 });
 
